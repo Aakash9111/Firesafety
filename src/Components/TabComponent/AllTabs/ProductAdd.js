6689257b10b93767d323c348e6115c9ef8560694
Aakash Dubey <aakashdubey20010907@gmail.com>
2023-01-24 13:09:56 +0530

    added import btn

--- src/Components/TabComponent/AllTabs/ProductAdd.js
+++ src/Components/TabComponent/AllTabs/ProductAdd.js
@@ -2,8 +2,14 @@ import React from "react";
 const Add = () => {
   return (
   <div className="contianer">
-      <div className="container">
-      <h5 className=" py-4">Add New Product</h5>
+      <div className="row mx-5">
+      <h5 className="col py-4">Add New Product</h5>
+      <form className="row py-4 col-sm-4">
+      <div class="input-group">
+      <input type="file" class="form-control" aria-describedby="inputGroupFileAddon04" />
+      <button class="btn btn-primary" type="button" id="inputGroupFileAddon04">Import</button>
+    </div>
+      </form>
       </div>
       <div className="container">
         <form method="post"> 
@@ -12,7 +18,7 @@ const Add = () => {
                 <label><b>Product Name:</b></label>
                 <div>
                 <input className="form-control" type="text" />
-                </div>
+                </div>  
               </div>
 
               <div className="col my-3">
