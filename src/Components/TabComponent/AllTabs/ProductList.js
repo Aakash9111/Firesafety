1ce13def28fd5dbab6b1f10c59dd32c9fbc88e43
Aakash Dubey <aakashdubey20010907@gmail.com>
2023-01-24 17:33:12 +0530

    product list footer

--- src/Components/TabComponent/AllTabs/ProductList.js
+++ src/Components/TabComponent/AllTabs/ProductList.js
@@ -23,6 +23,20 @@ const List = () => {
             </div>
         </div>
       </header> 
+
+      <footer className="row mx-3 py-4" >
+              <div className="col-lg-6" >
+                <label className=" my-2 p-2">Showing 1 of 5 of 5 entries</label>
+              </div>
+              <div className="col-lg-6">
+               <div className="row my-2">
+               <label className="col p-2"></label>
+                <label className="col-sm-2 p-2">Previous</label>
+                <button className="btn btn-primary col-sm-1 mx-1">1</button>
+                <label className="col-sm-1 p-2">Next</label>
+                </div>
+              </div>
+      </footer>
    </>
   );
 };
