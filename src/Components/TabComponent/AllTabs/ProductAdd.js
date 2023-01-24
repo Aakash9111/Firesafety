6a8609857a2150306f80d176cf429ad50e86736f
Aakash Dubey <aakashdubey20010907@gmail.com>
2023-01-24 14:58:16 +0530

    ui updated

--- src/Components/TabComponent/AllTabs/ProductAdd.js
+++ src/Components/TabComponent/AllTabs/ProductAdd.js
@@ -6,8 +6,8 @@ const Add = () => {
       <h5 className="col py-4">Add New Product</h5>
       <form className="row py-4 col-sm-4">
       <div class="input-group">
-      <input type="file" class="form-control" aria-describedby="inputGroupFileAddon04" />
-      <button class="btn btn-primary" type="button" id="inputGroupFileAddon04">Import</button>
+      <input type="file" class="form-control" />
+      <button class="btn btn-primary" type="button" >Import</button>
     </div>
       </form>
       </div>
@@ -17,14 +17,14 @@ const Add = () => {
               <div className="col my-3">
                 <label><b>Product Name:</b></label>
                 <div>
-                <input className="form-control" type="text" />
+                <input className="form-control" type="text" required/>
                 </div>  
               </div>
 
               <div className="col my-3">
                 <label><b>Manufactured Date:</b></label>
                 <div>
-                <input className="form-control" type="date" />
+                <input className="form-control" type="date" required/>
                 </div>
               </div>
           </div>
@@ -33,29 +33,28 @@ const Add = () => {
            <div className="col my-3">
                 <label><b>Extingusher Type:</b></label>
                 <div>
-                <input className="form-control" type="text" />
+                <input className="form-control" type="text" required/>
                 </div>
            </div>
            <div className="col my-3">
                 <label><b>Extingusher Next Due Date:</b></label>
                 <div>
-                <input className="form-control" type="date" />
+                <input className="form-control" type="date" required/>
                 </div>
               </div>
         </div>
 
         <div className="row">
-
               <div className="col my-3">
                 <label><b>Extingusher Capacity:</b></label>
                 <div>
-                <input className="form-control" type="text" />
+                <input className="form-control" type="text" required/>
                 </div>
               </div> 
               <div className="col my-3">
                 <label><b>Remark:</b></label>
                 <div>
-                <textarea className="w-100"></textarea>
+                <textarea className="w-100" required></textarea>
                 </div>
               </div>
         </div>
