94dbf891d27effed563deb25602b55301edc2130
Aakash Dubey <aakashdubey20010907@gmail.com>
2023-02-11 13:15:48 +0530

    product list ui

--- src/Components/TabComponent/AllTabs/ProductAdd.js
+++ src/Components/TabComponent/AllTabs/ProductAdd.js
@@ -1,31 +1,81 @@
 import React from "react";
+import { useState } from "react";
+
+
 const Add = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data= {
+     "product_name": name,
+    "product_extinguisher_type": exttype,
+     "product_extinguisher_capacity": extcap,
+     "product_manufactured_date": new Date(mfgdate),
+    "product_due_date": new Date(extduedate),
+    "product_remarks": remark
+    }
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    };
+    fetch('http://127.0.0.1:5000/api/product/create', requestOptions)
+        .then((response) => {
+          if(response.status===200){
+            alert("Product Saved")
+            setExtcap("")
+            setExtduedate(new Date(""))
+            setExttype("")
+            setName("")
+            setRemark("")
+            setMfgdate(new Date(""))
+          }else{
+            alert("Something went Wrong ")
+          }
+        });
+   };
+  
+  const [name,setName]= useState("");
+  const [mfgdate,setMfgdate]= useState("");
+  const [exttype,setExttype]= useState("");
+  const [extduedate,setExtduedate]= useState("");
+  const [extcap,setExtcap]= useState("");
+  const [remark,setRemark]= useState("");
+ const namechange = (event)=>{ setName(event.target.value); }
+ const mfgdatechange = (event)=>{ setMfgdate(event.target.value); }
+ const exttypechange = (event)=>{ setExttype(event.target.value); }
+ const extduedatechange = (event)=>{ setExtduedate(event.target.value); }
+ const extcapchange = (event)=>{ setExtcap(event.target.value); }
+ const remarkchange = (event)=>{ setRemark(event.target.value); }
+
+
+
   return (
   <div className="contianer">
       <header className="row mx-5">
         <h5 className="col py-4">Add New Product</h5>
         <form className="row py-4 col-sm-4">
-        <div class="input-group">
-        <input type="file" accept=".csv" class="form-control" />
-        <button class="btn btn-primary" type="button" >Import</button>
+        <div className="input-group">
+        <input type="file" accept=".csv" className="form-control" />
+        <button className="btn btn-primary" type="button" >Import</button>
         </div>
         </form>
       </header>
      
       <div className="container">
-        <form method="post"> 
+        <form onSubmit={handleSubmit}> 
           <div className="row">
               <div className="col my-3">
                 <label><b>Product Name:</b></label>
                 <div>
-                <input className="form-control" type="text" required/>
+                <input name="name"  value={name} onChange={namechange} className="form-control" type="text" required/>
                 </div>  
               </div>
 
               <div className="col my-3">
                 <label><b>Manufactured Date:</b></label>
                 <div>
-                <input className="form-control" type="date" required/>
+                <input name="mfgdate" value={mfgdate} onChange={mfgdatechange}  className="form-control" type="date" required/>
                 </div>
               </div>
           </div>
@@ -34,13 +84,13 @@ const Add = () => {
            <div className="col my-3">
                 <label><b>Extingusher Type:</b></label>
                 <div>
-                <input className="form-control" type="text" required/>
+                <input name="exttype" value={exttype}  onChange={exttypechange} className="form-control" type="text" required/>
                 </div>
            </div>
            <div className="col my-3">
                 <label><b>Extingusher Next Due Date:</b></label>
                 <div>
-                <input className="form-control" type="date" required/>
+                <input name="extduedate"  value={extduedate} onChange={extduedatechange} className="form-control" type="date" required/>
                 </div>
               </div>
         </div>
@@ -49,19 +99,19 @@ const Add = () => {
               <div className="col my-3">
                 <label><b>Extingusher Capacity:</b></label>
                 <div>
-                <input className="form-control" type="text" required/>
+                <input name="extcap"   value={extcap} onChange={extcapchange} className="form-control" type="text" required/>
                 </div>
               </div> 
               <div className="col my-3">
                 <label><b>Remark:</b></label>
                 <div>
-                <textarea className="w-100" required></textarea>
+                <textarea name="remark" value={remark}  rows='4' onChange={remarkchange} className="w-100" required></textarea>
                 </div>
               </div>
         </div>
          
         <div className="text-center my-2">
-                <button type='submit' className="btn btn-primary">Save Data</button>
+            <button type="submit"  className="btn btn-primary">Save Data</button>
         </div>    
         </form>
       </div>
--- src/Components/TabComponent/AllTabs/ProductList.js
+++ src/Components/TabComponent/AllTabs/ProductList.js
@@ -1,12 +1,61 @@
-import { useState } from "react";
+import { useEffect,useState } from "react";
 import React from "react";
+import axios from "axios";
 
 const List = () => {
-
-  const [entries,setEntries]= useState()
+  const [entries,setEntries]= useState(5)
   const changeEntries =(event)=>{
     setEntries(event.target.value)
+    setEnd(event.target.value)
+    
   }
+
+  const [items, setItems] = useState([]);  
+  useEffect(() => {
+    const fetchProducts = async () => {
+       const res = await axios.get("http://localhost:5000/api/product/list");              
+       setItems(res.data['products']);              
+    };
+    fetchProducts();
+  },[]);
+
+
+  const [current_page, setCurrentPage] = useState(1);
+  const [start,setStart] = useState(0); 
+  const [end,setEnd]= useState(entries);
+  const results = [];
+
+  const onnext =()=>{
+       if(current_page < total_pages){
+          setCurrentPage(current_page+1)
+          setStart(end)
+          setEnd(parseInt(end)+parseInt(entries))
+       }
+  }
+  const onprevious =()=>{
+    if(current_page>1){
+      setCurrentPage(current_page-1)
+      setStart(start-entries)
+      setEnd(end-entries)
+    }
+  }
+
+  items.forEach(item => {
+    results.push(
+      <tr key={item._id}>
+        <td>{item._id}</td>
+        <td>{item.product_name}<br/>
+        {item.product_extinguisher_type} {item.product_extinguisher_capacity}<br/>
+        {item.product_manufactured_date.slice(0,10)} | {item.product_due_date.slice(0,10)}</td>
+        <td>{item.product_extinguisher_type}</td>
+        <td>
+          <button className="btn btn-primary mx-1" ><i className="bi bi-pencil-square"></i></button>
+          <button className="btn btn-danger mx-1"><i className="bi bi-trash"></i></button>
+        </td>
+      </tr>
+    );
+  });
+  const total_pages= Math.ceil(results.length/entries)
 
   return (
    <>
@@ -20,7 +69,8 @@ const List = () => {
               <option value="15">15</option>
               <option value="20">20</option>
             </select>
-            <labels className="p-2" >entries</labels>
+            <label className="p-2" >entries</label>
+          
             </div>
         </div>
         <div className="col-lg-6">
@@ -33,42 +83,38 @@ const List = () => {
       </header> 
 
       <div className="mx-3 py-2">
-      <table class="table">
+
+      <table className="table">
         <thead>
           <tr className="border border-grey">
-            <th className="col-lg-2" scope="col">ID</th>
+            <th className="col-lg-3" scope="col">ID</th>
             <th className="col-lg-5" scope="col">Product</th>
-            <th className="col-lg-3" scope="col">Type</th>
-            <th className="col-lg-3" scope="col">Action</th>
+            <th className="col-lg-2" scope="col">Type</th>
+            <th className="col-lg-2" scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>extingusher</td>
-            <td>extingusher</td>
-            <td>
-              <button className="btn btn-primary mx-1" ><i class="bi bi-pencil-square"></i></button>
-              <button className="btn btn-danger mx-1"><i class="bi bi-trash"></i></button>
-            </td>
-          </tr>
-          
+        {results.slice(start,end)}
         </tbody>
       </table>
       </div>
-      
 
       <footer className="row mx-3 py-4" >
               <div className="col-lg-6" >
-                <label className=" my-2 p-2">Showing 1 of 5 of {entries} entries</label>
+                <label className=" my-2 p-2">Showing {current_page} of {total_pages} of {entries} entries</label>
               </div>
               <div className="col-lg-6">
                <div className="row my-2">
                <label className="col p-2"></label>
-                <label className="col-sm-2 p-2">Previous</label>
-                <button className="border border-grey rounded col-sm-1 ">1</button>
-                <label className="col-sm-1 p-2">Next</label>
-                </div>
+               { current_page>1 &&
+                 <label onClick={onprevious} className="col-sm-2 p-2">Previous</label>   
+                }
+                <button className="border border-grey rounded col-sm-1 ">{current_page}</button>
+                { current_page!==total_pages &&
+                   <label onClick={onnext}  className="col-sm-1 p-2">Next</label>
+                }
+               
+                  </div>
               </div>
       </footer>
    </>
